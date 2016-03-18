02c24820fead47f1a6f5b177541b02324dd4576c
JS4lcaEq <condorrr@gmail.com>
2016-03-18 20:45:34 +0300

    9

--- Coursera4/app/app.js
+++ Coursera4/app/app.js
@@ -7,10 +7,20 @@ angular.module('app', ['ngRoute']);
         this.test = "test";
         this.location = $location;
         this.route = $route;
+        this.current = false;
         this.images = [
             { p: "av_h_s1.jpg", v: "27S.jpg" },
             { p: "9S_.jpg", v: "9S.jpg" }
         ];
+        this.open = function (item) {
+            var self = this;
+            self.current = item;
+        };
+        this.close = function () {
+            var self = this;
+            self.current = false;
+        };
+
     }
 
     angular.module('app').controller('MainCtrl', MainCtrl);
