e01517a056812a36a083fbe6e47688b87b6018f6
JS4lcaEq <condorrr@gmail.com>
2016-03-18 17:20:59 +0300

    5

---
+++ Coursera4/app/app.js
@@ -0,0 +1,19 @@
+﻿
+angular.module('app', ['ngRoute']);
+
+(function () {
+
+    function MainCtrl($route, $routeParams, $location, $http, TreeService) {
+        this.test = "test";
+        this.location = $location;
+        this.route = $route;
+    }
+
+    angular.module('app').controller('MainCtrl', MainCtrl);
+
+})();
+
+//angular.bootstrap("html", ['app']);
+
+
+
