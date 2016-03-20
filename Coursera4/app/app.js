9d35e43725033952e8b7e6150a94b806bd32f5b7
Andrey Vasiliev <andvas@outlook.com>
2016-03-20 22:23:43 +0300

    435

--- Coursera4/app/app.js
+++ Coursera4/app/app.js
@@ -3,26 +3,37 @@ angular.module('app', ['ngRoute']);
 
 (function () {
 
+    function _multiple(arr) {
+        var temp = [];
+        for (var i = 0; i < arr.length; i++) {
+            temp.push({ p: arr[i].p, v: arr[i].v });
+        }
+        console.log(temp);
+        for (var i = 0; i < temp.length; i++) {
+            arr.push(temp[i]);
+        }
+        console.log(arr);
+    }
+
     function MainCtrl($route, $routeParams, $location) {
-        this.test = "test";
+        this.base = "http://js4lcaeq.github.io/Coursera4/Coursera4/images/";
         this.location = $location;
         this.route = $route;
         this.current = false;
         this.images = [
-            { p: "images/av_h_s1.jpg", v: "images/27S.jpg" },
-            { p: "images/11S_.jpg", v: "images/11S.jpg" },
-            { p: "images/16S_.jpg", v: "images/16S.jpg" },
-            { p: "images/28S_.jpg", v: "images/28S.jpg" },
-            { p: "images/29S_.jpg", v: "images/29S.jpg" },
-            { p: "images/3S_.jpg", v: "images/3S.jpg" },
-            { p: "images/4S_.jpg", v: "images/4S.jpg" },
-            { p: "images/9S_.jpg", v: "images/9S.jpg" }
+            { p: "av_h_s1.jpg", v: "27S.jpg" },
+            { p: "11S_.jpg", v: "11S.jpg" },
+            { p: "16S_.jpg", v: "16S.jpg" },
+            { p: "28S_.jpg", v: "28S.jpg" },
+            { p: "29S_.jpg", v: "29S.jpg" },
+            { p: "3S_.jpg", v: "3S.jpg" },
+            { p: "4S_.jpg", v: "4S.jpg" },
+            { p: "9S_.jpg", v: "9S.jpg" }
         ];
+
         this.open = function (item) {
             var self = this;
             self.current = item;
-            self.current.maxWidth = $("html").width();
-            self.current.maxHeight = $("html").height();           
         };
         this.close = function () {
             var self = this;
@@ -33,32 +44,40 @@ angular.module('app', ['ngRoute']);
 
     angular.module('app').controller('MainCtrl', MainCtrl);
 
-})();
-//style="max-width:{{ctrl.current.maxWidth}}px; max-height:{{ctrl.current.maxHeight}}px;"
-angular.module('app').config(function ($routeProvider) {
+    function Router($routeProvider) {
+        $routeProvider
 
-    $routeProvider
-
-        .when('/home', {
-            template:
-'<div id="view-image-box" ng-click="ctrl.close()" ng-if="ctrl.current" >' +
-    '<img src="{{ctrl.current.v}}" alt="big view" title="click me"/>' +
-'</div>'+
-'<div class="container-fluid">'+
-    '<div class="row gallery-box">'+
-        '<div class="col-xs-6 col-sm-3 col-md-2 " ng-repeat="item in ctrl.images">'+
-            '<img src="{{item.p}}" alt="preview" class="img-thumbnail" ng-click="ctrl.open(item)" title="click me" />'+
-        '</div>'+
-    '</div>'+
+            .when('/home', {
+                template:
+'<div ng-if="ctrl.current"  id="view-image-box" ng-click="ctrl.close()">' +
+'  <img ng-if="ctrl.current"  ng-src="{{ctrl.base}}{{ctrl.current}}" alt="big view" title="click me"/>' +
+'</div>' +
+'<div class="container-fluid">' +
+'  <div class="row gallery-box">' +
+'    <div class="col-xs-6 col-sm-3 col-md-2 " ng-repeat="item in ctrl.images">' +
+'      <img ng-src="{{ctrl.base}}{{item.p}}" alt="preview" class="img-thumbnail" ng-click="ctrl.open(item.v)" title="click me" />' +
+'    </div>' +
+'  </div>' +
 '</div>'
-        })
+            })
 
-        .when('/about', {
-            template: '<div class="jumbotron"><div class="container"><h1>Simple Gallery</h1><p>Made for "Advanced Styling with Responsive Design" course by Colleen van Lent, Ph.D. from University of Michigan.</p><p>Powered by Bootstrap, jQuery and AngularJS.</p></div></div>'
-        })
+            .when('/about', {
+                template:
+'<div class="jumbotron">' +
+'  <div class="container">' +
+'    <h1>Simple Gallery</h1>' +
+'    <p>Made for "Advanced Styling with Responsive Design" course by Colleen van Lent, Ph.D. from University of Michigan.</p>' +
+'    <p>Powered by Bootstrap, jQuery and AngularJS.</p>' +
+'  </div>' +
+'</div>'
+            })
 
-        .otherwise('/home');
-});
+            .otherwise('/home');
+    }
+
+    angular.module('app').config(Router);
+
+})();
 
 //angular.bootstrap("html", ['app']);
 
---
+++ Coursera4/app/service.js
@@ -0,0 +1,123 @@
+﻿(function () {
+
+    function service() {
+
+
+
+        function _createTestData(item, branchLength, levelsLength) {
+            item.items.length = 0;
+            function step(level, parent) {
+                level++;
+                for (var i = 0; i < branchLength; i++) {
+                    var tm = {};
+                    var id = item.items.length + 1;
+                    tm[item.idFieldName] = id;
+                    tm[item.idpFieldName] = parent;
+                    tm[item.nameFieldName] = item.nameFieldName + "_" + id;
+                    item.items.push(tm);
+                    if (level < levelsLength) {
+                        step(level, id);
+                    }
+                }
+            };
+
+            step(0, 0);
+        }
+
+        function _setIndex(item) {
+            var index = {};
+            for (var i = 0; i < item.items.length; i++) {
+                var id = item.items[i][item.idFieldName];
+                index[id] = i;
+            }
+            item.index = index;
+        }
+
+        function _setBranches(item) {
+            var branches = {};
+            for (var i = 0; i < item.items.length; i++) {
+                var idp = item.items[i][item.idpFieldName];
+                var branch = branches[idp];
+                if (!branch) {
+                    branch = { items: [], opened: false, level: 0 };
+                    branches[idp] = branch;
+                }
+                branch.items.push(item.items[i]);
+            }
+            item.branches = branches;
+        }
+
+        function _setLevels(item) {
+
+            function step(id, level) {
+                var branch = item.branches[id];
+                if (branch) {
+                    branch.level = level;
+                    for (var i = 0; i < branch.items.length; i++) {
+                        var idSub = branch.items[i][item.idFieldName];
+                        step(idSub, level + 1);
+                    }
+                }
+            }
+            step(item.idRootBranch, 0);
+        }
+
+        function _open(item, id) {
+            if (item.branches[id]) {
+                item.branches[id].opened = true;
+            }
+            var parent = item.items[item.index[id]];
+            if (parent) {
+                var idp = parent[item.idpFieldName];
+                if (idp != id) {
+                    _open(item, idp);
+                }
+            }
+        }
+
+
+        function _close(item) {
+            for (var i in item.branches) {
+                item.branches[i].opened = false;
+            }
+        }
+
+        this.items = [];
+
+        this.get = function (id) {
+            var self = this;
+            if (!id) {
+                id = self.items.length;
+                self.items.push({
+                    id: id, idFieldName: "id", idpFieldName: "idp", nameFieldName: "nm", idRootBranch: 0
+                    , items: [], index: {}, branches: {}
+                    , createTestData: function (branchLength, levelsLength) { _createTestData(this, branchLength, levelsLength); }
+                    , setIndex: function () { _setIndex(this); }
+                    , setBranches: function () { _setBranches(this); }
+                    , setLevels: function () { _setLevels(this); }
+                    , open: function (id) { _open(this, id); }
+                    , close: function () { _close(this); }
+                });
+            }
+            return self.items[id];
+
+        };
+
+        this.test = function () { return "treeServiceTest"; };
+
+        this.parseBranches = function (items) {
+            var branches = {};
+            for (var i = 0; i < items.length; i++) {
+                if (!branches[items[i].idp]) {
+                    branches[items[i].idp] = { list: [], opened: false };
+                }
+                branches[items[i].idp].list.push(items[i]);
+            }
+            return branches;
+        };
+
+    }
+
+    angular.module('app').service('TreeService', service);
+
+})();
